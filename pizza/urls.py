e0f80a25ca9484deaa5586b810e7fac46cd08856
tarik <63872091+tariksitki@users.noreply.github.com>
2022-06-14 21:09:08 +0200

    ...

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import home
+from .views import home, numberOfPizza, order
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("order/", order, name="order"),
+    path("pizzas/", numberOfPizza, name="pizzas"),
 ]
--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,5 @@
 from unicodedata import name
+from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import PizzaForm
 
@@ -18,3 +19,24 @@ def order(request):
     }
 
     return render(request, "pizza/order.html", context)
+
+
+
+
+
+
+def numberOfPizza(request):
+    # print(int(request.GET.get("number")))
+    form = PizzaForm()
+    number_pizza = int(request.GET.get("number"))
+    mylist = [1 for i in range(number_pizza)]
+    print(mylist)
+
+
+    context = {
+        "mylist" : mylist,
+        "form" : form
+
+    }
+
+    return render(request, "pizza/pizzas.html", context)
